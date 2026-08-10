a20abe714284bf0edc3c865ad8096068dcf56424
Tvman <elijahpauley702@gmail.com>
2026-08-09 20:26:02 -0400

    Update MainActivity.java

--- app/src/main/java/com/vr/shizuku/manager/MainActivity.java
+++ app/src/main/java/com/vr/shizuku/manager/MainActivity.java
@@ -7,5 +7,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Links this activity to activity_main.xml so it renders UI instead of a white screen
+        setContentView(R.layout.activity_main); 
     }
 }
